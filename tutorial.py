ce503785274ef7041096091f168c1d9ecdfa0a4d
joshrubin48 <96543029+joshrubin48@users.noreply.github.com>
2022-08-22 21:36:12 +0300

    Joshys first commit, such a sweet and charming boy

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+from cgitb import small
+
+
+
+balls = int(input("Select the number of balls you want to suck on: "))
+if balls <= 2:
+    print("You're not that ghey")
+elif balls >= 3 and balls <= 10:
+    print("you sucked on many balls today, ur ghey")
+elif balls >= 10:
+    print("You must be Spencer Taint :PP")
+
